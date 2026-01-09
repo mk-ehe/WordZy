557b8783792acbe28e8bfb05eaa55b2b18c09230
Mateusz <kontakt.mateusz.kudas@gmail.com>
2026-01-10 00:54:30 +0100

    enter

--- Entry.py
+++ Entry.py
@@ -1,5 +1,6 @@
 from PySide6.QtWidgets import QWidget, QVBoxLayout, QLabel, QLineEdit, QPushButton, QHBoxLayout
 from PySide6.QtCore import Qt, Signal
+
 import database
 
 class EntryScreen(QWidget):
